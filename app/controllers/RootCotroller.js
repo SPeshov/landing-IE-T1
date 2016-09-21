23464e9d95b73308dce473a9a1f75a7186e726b2
Stojan Peshov <stojan.peshov@gmail.com>
2016-09-21 19:32:01 +0200

    anchorScroll added

--- app/controllers/RootCotroller.js
+++ app/controllers/RootCotroller.js
@@ -1,7 +1,14 @@
-WPAngularStarter.controller('RootController', function($scope, $state, $stateParams) {
+WPAngularStarter.controller('RootController', function($scope, $state, $stateParams,  $anchorScroll, $location) {
   $scope.isActive = false;
     $scope.activeButton = function() {
       $scope.isActive = !$scope.isActive;
 
     };
+
+    $scope.scrollTo = function(id) {
+     $location.hash(id);
+     $anchorScroll();
+  };
+
+
 });
